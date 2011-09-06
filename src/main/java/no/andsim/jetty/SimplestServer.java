8d72c4421e0fdd50d6468d1b51c2b5f2131f66c0
simon.andresen <Simon.Royrvik.Andresen@iteraconsulting.no>
2011-09-06 16:51:31 +0200

    Added embedded jetty

---
+++ src/main/java/no/andsim/jetty/SimplestServer.java
@@ -0,0 +1,22 @@
+package no.andsim.jetty;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+public class SimplestServer
+{
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(9090);
+        
+        WebAppContext context = new WebAppContext();
+        context.setResourceBase("../vareservice/src/main/webapp");
+        context.setContextPath("/");
+        context.setParentLoaderPriority(true);
+ 
+        server.setHandler(context);
+        
+        server.start();
+        server.join();
+        
+    }
+}
